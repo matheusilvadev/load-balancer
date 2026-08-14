3653e81a4a595fa6f77eb131cdf3caae5dafe9c9
devma <eurotheus@gmail.com>
2026-08-14 10:35:08 -0300

    feat: bootstrap load balancer HTTP server
    - Configure backend endpoints through BackendRegistry
    - Initialize RoundRobinRoutingStrategy for backend selection
    - Configure HttpProxyClient for request forwarding
    - Wire application components through manual dependency injection
    - Initialize native Java HTTP server on port 8080
    - Register ProxyHandler as the root request handler
    - Configure a fixed thread pool for concurrent request processing

--- src/main/java/com/gt/Main.java
+++ src/main/java/com/gt/Main.java
@@ -1,9 +1,38 @@
 package com.gt;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.gt.backend.BackendRegistry;
+import com.gt.handler.ProxyHandler;
+import com.gt.proxy.HttpProxyClient;
+import com.gt.routing.RoundRobinRoutingStrategy;
+import com.gt.routing.RoutingStrategy;
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+
+        //Manual Configuration and Dependency Injection
+        BackendRegistry registry = new BackendRegistry(List.of(
+                "http://localhost:8081",
+                "http://localhost:8082",
+                "http://localhost:8083"
+        ));
+
+        RoutingStrategy strategy = new RoundRobinRoutingStrategy();
+        HttpProxyClient proxyClient = new HttpProxyClient();
+
+        ProxyHandler proxyHandler = new ProxyHandler(registry, strategy, proxyClient);
+
+        // Server Bootstrap (native)
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.setExecutor(Executors.newFixedThreadPool(20)); // Thread Pool (isolated)
+        server.createContext("/", proxyHandler);
+
+        System.out.println("Modular Load Balancer running on port " + port + "...");
+        server.start();
     }
 }
